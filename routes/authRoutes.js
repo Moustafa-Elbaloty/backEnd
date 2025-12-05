a02536d1773bae67711246bbe2c6288ff61ffd9e
Moustafa elbaloty <moustafaelbaloty@gmail.com>
2025-12-05 17:57:03 +0200

    Add email verification route to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  verifyEmail,
+} = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
+router.get("/verify-email/:token", verifyEmail);
 
 
 router.get("/profile", protect, (req, res) => {
