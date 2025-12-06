393f20d7a50d45dfa2e081ff37c53cd6ae27cdf6
Badr_Korim <baderkorim@gmail.com>
2025-12-07 01:07:53 +0200

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -7,8 +7,6 @@ const {
   addToCart,
   removeFromCart,
   updateCartItem,
-  getAllCarts,
-  deleteCart,
 } = require("../controllers/cartController");
 
 const { authorizeRole } = require("../middleware/roleMiddleware");
@@ -17,25 +15,16 @@ const { authorizeRole } = require("../middleware/roleMiddleware");
 const { protect } = require("../middleware/authMiddleware");
 
 // GET /api/cart/
-router.get("/:userId", protect,authorizeRole("admin"), getCart);
 router.get("/", protect, getCart);
 
-// GET /api/cart/allCarts
-router.get("/allCarts", protect, authorizeRole("admin"), getAllCarts);
-
-// DELETE /api/cart/delete/:cartId
-router.delete("/delete/:cartId", protect, authorizeRole("admin"), deleteCart); 
 
 // POST /api/cart/add
-router.post("/add/:userId", protect, authorizeRole("admin"), addToCart);
 router.post("/add", protect, addToCart);
 
 // PUT /api/cart/update/:productId
-router.put("/update/:productId/:userId", protect, authorizeRole("admin"), updateCartItem);
 router.put("/update/:productId", protect, updateCartItem);
 
 // DELETE /api/cart/remove/:productId
-router.delete("/remove/:productId/:userId", protect, authorizeRole("admin"), removeFromCart);
 router.delete("/remove/:productId", protect, removeFromCart);
 
 
