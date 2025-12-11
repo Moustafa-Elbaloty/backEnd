1c8183e7c1b6df5b75257c887a89199f7726ff04
Badr_Korim <baderkorim@gmail.com>
2025-12-11 14:53:16 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,8 +8,12 @@ const { updateUser, deleteUser } = require("../controllers/userController");
 // 🔹 تحديث مستخدم
 router.put("/update", protect, authorizeRole("user"), updateUser);
 
+// 🔹 جلب بيانات المستخدم الحالي
+router.get("/dashboard", protect, authorizeRole("user"), getUserDashboard)
+
 // 🔹 حذف مستخدم
 router.delete("/delete/", protect, authorizeRole("user"), deleteUser);
 
 module.exports = router;
+
 
