08f46efe6643532cd9a510e71a472d60c74e28bd
Moustafa elbaloty <moustafaelbaloty@gmail.com>
2025-12-16 17:36:14 +0200

    feat: implement user authentication routes for registration, login, password management, email verification, and Google login.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, verifyEmail, resetPassword, forgotPassword, changePassword, logout } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
+const { googleLogin } = require("../controllers/authController");
 
 router.post("/register", registerUser);
 
@@ -17,6 +18,8 @@ router.put("/change-password", protect, changePassword);
 
 router.post("/logout", protect, logout);
 
+
+router.post("/google", googleLogin);
 
 
 module.exports = router;
