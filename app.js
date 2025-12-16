9d6de277e3a0f299a0e8dd1a09e669ad11dd4dd3
Moustafa elbaloty <moustafaelbaloty@gmail.com>
2025-12-16 12:26:41 +0200

    feat: Enable URL-encoded request body parsing.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Make uploads folder public
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
